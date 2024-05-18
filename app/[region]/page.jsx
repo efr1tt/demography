2ab71a9a273da23f0af50827a911a0c9628496dc
Maxim Esenov <ef-@MacBook-Air-Maxim.local>
2024-05-18 15:11:53 +0300

    add highcharts growth column type

--- app/[region]/page.jsx
+++ app/[region]/page.jsx
@@ -6,7 +6,9 @@ import Highcharts from "highcharts"
 import HighchartsReact from "highcharts-react-official"
 
 const Regions = (props) => {
-  const [chartData, setChartData] = useState([])
+  const [populationData, setPopulationData] = useState([])
+  const [growthData, setGrowthData] = useState([])
+  const [percentageData, setPercentageData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [data, setData] = useState([])
   let regionId = Number(props.params.region.slice(6))
@@ -19,20 +21,29 @@ const Regions = (props) => {
         .eq("region_id", regionId)
 
       if (!error && data) {
-        console.log("data: ", data)
-        const transformedData = data.map((item) => ({
+        const transformedPopulationData = data.map((item) => ({
           x: item.year,
           y: item.population,
           name: String(item.year),
         }))
-        setChartData(transformedData)
-        console.log("chartData: ", chartData)
+        const transformedGrowthData = data.map((item) => ({
+          x: item.year,
+          y: item.growth,
+          name: String(item.year),
+        }))
+        const transformedPercentageData = data.map((item) => ({
+          x: item.year,
+          y: item.percentage_of_the_urban_population,
+          name: String(item.year),
+        }))
+        setPopulationData(transformedPopulationData)
+        setGrowthData(transformedGrowthData)
+        setPercentageData(transformedPercentageData)
       }
       setIsLoading(false)
     }
 
     fetchData()
-    console.log("data: ", data[0])
   }, [regionId])
 
   const chartOptionsPopulation = {
@@ -40,7 +51,7 @@ const Regions = (props) => {
       type: "line",
     },
     title: {
-      text: "Численность населения:",
+      text: "Численность постоянного населения в среднем за год:",
     },
     xAxis: {
       title: {
@@ -60,7 +71,84 @@ const Regions = (props) => {
     series: [
       {
         name: "Data",
-        data: chartData,
+        data: populationData,
+        dataLabels: {
+          enabled: true,
+          formatter: function () {
+            return Highcharts.numberFormat(this.y, 0, ",", " ")
+          },
+        },
+      },
+    ],
+    credits: {
+      enabled: false,
+    },
+  }
+  const chartOptionsGrowth = {
+    chart: {
+      type: "column",
+    },
+    title: {
+      text: "Общий прирост постоянного населения:",
+    },
+    xAxis: {
+      title: {
+        text: "",
+      },
+      type: "category",
+    },
+    yAxis: {
+      title: {
+        text: null, // Убираем название
+      },
+      labels: {
+        enabled: false, // Убираем метки
+      },
+      gridLineWidth: 0, // Убираем линии сетки
+    },
+    series: [
+      {
+        name: "Data",
+        data: growthData,
+        dataLabels: {
+          enabled: true,
+          formatter: function () {
+            return Highcharts.numberFormat(this.y, 0, ",", " ")
+          },
+        },
+      },
+    ],
+    credits: {
+      enabled: false,
+    },
+  }
+
+  const chartOptionsPercentage = {
+    chart: {
+      type: "column",
+    },
+    title: {
+      text: "Общий прирост постоянного населения:",
+    },
+    xAxis: {
+      title: {
+        text: "",
+      },
+      type: "category",
+    },
+    yAxis: {
+      title: {
+        text: null, // Убираем название
+      },
+      labels: {
+        enabled: false, // Убираем метки
+      },
+      gridLineWidth: 0, // Убираем линии сетки
+    },
+    series: [
+      {
+        name: "Data",
+        data: percentageData,
         dataLabels: {
           enabled: true,
           formatter: function () {
@@ -84,6 +172,14 @@ const Regions = (props) => {
             highcharts={Highcharts}
             options={chartOptionsPopulation}
           />
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={chartOptionsGrowth}
+          />
+          {/* <HighchartsReact
+            highcharts={Highcharts}
+            options={chartOptionsPercentage}
+          /> */}
         </div>
       )}
     </div>
